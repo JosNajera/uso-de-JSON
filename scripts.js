3b27f60867d468c2ff0ce55a1c684dc267c2eb29
JosNajera <125822784+JosNajera@users.noreply.github.com>
2024-09-11 10:38:13 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,54 +1,81 @@
-fetch('https://xmljatsredalyc.org//wp-json/wp/v2/posts')
-  .then(res => res.json())
-  .then(posts => {
-    posts.forEach((post, index) => {
-      if (post.excerpt && post.excerpt.rendered && post.link) {
-        const liga = post.link;
-        const parrafoRen = stripHTML(post.excerpt.rendered);
-        const quitar = 'Continuar leyendo →'
-        const parrafoRendered = removeSubstring(parrafoRen, quitar)
-        const parrafoId = `intro-${index + 1}`;
-        const parrafo = document.getElementById(parrafoId);
-
-        if (parrafo) {
-          const salto = document.createElement('br');
-          const url = document.createElement('a');
-          url.href = liga;
-          url.textContent = "Continuar leyendo →";
-          url.target = '_blank';
-          url.className = 'cuerpoLink';
-          parrafo.innerHTML = parrafoRendered;
-          parrafo.appendChild(salto);
-          parrafo.appendChild(url);
+function pagina(num) {
+    if (num === 1) {
+        absorber('https://xmljatsredalyc.org/wp-json/wp/v2/posts');
+    } else
+        if (num === 2) {
+            absorber('https://xmljatsredalyc.org//wp-json/wp/v2/posts?page=2')
         }
-      }
 
-      if (post.title && post.title.rendered && post.link) {
-        const titleRendered = post.title.rendered;
-        const linkUrl = post.link
-        const titleId = `titulo-${index + 1}`;
-        const titulo = document.getElementById(titleId);
-        if (titulo) {
-          const enlace = document.createElement('a');
-          enlace.href = linkUrl;
-          enlace.textContent = titleRendered; // Establecer el texto del enlace
-          enlace.target = '_blank'; // Abrir en una nueva pestaña (opcional)
-          enlace.className = 'tituloLink';
-          // Limpiar el contenido del elemento antes de agregar el nuevo enlace
-          titulo.innerHTML = '';
-          titulo.appendChild(enlace);
-        }
-      }
-    });
-  })
-  .catch(error => console.error('Error:', error));
+}
 
+function absorber(liga) {
+    fetch(liga)
+        .then(res => res.json())
+        .then(posts => {
+            const contenedorID = "contenedor-tarjetas";
+            const contenedor = document.getElementById(contenedorID);
+            contenedor.innerHTML = "";
+
+            posts.forEach((post, index) => {
+                if (post.title && post.title.rendered && post.excerpt && post.excerpt.rendered && post.link) {
+                    const titleRendered = post.title.rendered;
+                    const excerptRendered = stripHTML(post.excerpt.rendered);
+                    const quitar = 'Continuar leyendo →';
+                    const parrafo = removeSubstring(excerptRendered, quitar);
+                    const link = post.link;
+
+                    //Contenedor de cada carta
+                    const carta = document.createElement('div');
+                    carta.className = "w3-card-4";
+                    carta.style = "width: 390px; height: fit-content;";
+                    carta.id = `intro-${index + 1}`;
+                    contenedor.appendChild(carta);
+
+                    //header de cada carta
+                    const headCarta = document.createElement('header');
+                    headCarta.className = "w3-container";
+                    carta.appendChild(headCarta);
+
+                    //titulo de las cartas, se inserta en el header, tiene un formato de link
+                    const tituloCarta = document.createElement('h3');
+                    headCarta.appendChild(tituloCarta);
+                    const url = document.createElement('a');
+                    url.href = link;
+                    url.textContent = titleRendered;
+                    url.className = "link-titulo";
+                    url.target = '_blank';
+                    tituloCarta.appendChild(url);
+
+                    //contenedor para insertar el texto de cada carta
+                    const cuerpoCarta = document.createElement('div');
+                    cuerpoCarta.className = "w3-container";
+                    cuerpoCarta.style = "padding: 35px;";
+                    cuerpoCarta.id = `cuerpo-${index + 1}`;
+                    carta.appendChild(cuerpoCarta);
+
+                    //texto de las cartas
+                    const linkTexto = document.createElement('a');
+                    linkTexto.href = link;
+                    linkTexto.textContent = 'Continuar leyendo →';
+                    linkTexto.className = 'link-texto';
+                    linkTexto.target = '_blank';
+                    const textoCarta = document.createElement('p');
+                    textoCarta.textContent = parrafo;
+                    textoCarta.className = "texto-cartas";
+                    cuerpoCarta.appendChild(textoCarta);
+                    cuerpoCarta.appendChild(linkTexto);
+
+                }
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
 function stripHTML(htmlString) {
-  const div = document.createElement('div');
-  div.innerHTML = htmlString;
-  return div.innerText || div.textContent || '';
+    const div = document.createElement('div');
+    div.innerHTML = htmlString;
+    return div.innerText || div.textContent || '';
 }
 
 function removeSubstring(text, substring) {
-  return text.replace(substring, '');
+    return text.replace(substring, '');
 }
